b197f987d84349efe143962d5b5d4df6a8ba6715
Korhan Sevinc <sevinckorhan1@gmail.com>
2024-07-17 23:13:08 +0300

    Update prompt_templates.py

--- prompt_templates.py
+++ prompt_templates.py
@@ -1,13 +1,16 @@
 ''' For English Models.'''
 
-
-summarize_prompt_template = """<s>[INST] You are a helpful asistant and your task is to find a title about the given input and only write down the title. Make sure that the title contains maximum 5 word. Do not specify any informations only find a title about the theme, and write it down. Make sure use ':' but Do not use any other special characters.
+# For chat-rag chain.
+memory_prompt_template = """<s>[INST] You are an AI chatbot having a conversation with a human. Answer his questions.
    Previous conversation: {history}
    Human: {human_input}
    AI: [/INST]"""
 
-
-
+# For summarize_chat_chain.
+summarize_prompt_template = """<s>[INST] You are a helpful asistant and your task is to find a title about the given input and only write down the title. Make sure that the title contains maximum 5 word. Do not specify any informations only find a title about the theme, and write it down. Make sure use ':' but Do not use any other special characters.
+   Previous conversation: {history}
+   Human: {human_input}
+   AI: [/INST]"""
 
 
 ''' For Turkish Models.'''
